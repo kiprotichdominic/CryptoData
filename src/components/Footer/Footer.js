22b54f36f01271ac0a5f31d42ee32e7044819e21
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-07-09 17:27:14 +0300

    add colored footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -65,18 +65,18 @@ const navigation = [
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 mt-5">
+        <footer className="mt-5 white mainbg-color">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
                 <div className="flex justify-center space-x-6 md:order-2">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                        <a key={item.name} href={item.href} className="text-white hover:text-white">
                             <span className="sr-only">{item.name}</span>
                             <item.icon className="h-6 w-6" aria-hidden="true" />
                         </a>
                     ))}
                 </div>
                 <div className="mt-8 md:mt-0 md:order-1">
-                    <p className="text-center text-base text-gray-400">&copy; 2022 CryptoData, Inc. All rights reserved.</p>
+                    <p className="text-center text-base text-white">&copy; 2022 CryptoData, Inc. All rights reserved.</p>
                 </div>
             </div>
         </footer>
