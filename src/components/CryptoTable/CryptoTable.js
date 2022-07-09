070db82e8803b6df5edddb63a865a86a05b7a97d
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-07-09 23:03:00 +0300

    remove pricekey function from cryptotable

--- src/components/CryptoTable/CryptoTable.js
+++ src/components/CryptoTable/CryptoTable.js
@@ -105,12 +105,3 @@ export default function CryptoTable({ crypto, cryptoPrices }) {
         </div>
     )
 }
-
-
-function PriceKey({ currPrice, updatedIds, updatedPrices, coinId }) {
-    const index = updatedIds?.indexOf(coinId)
-    if (index != null && index >= 0) {
-        return updatedPrices[index]
-    }
-    return convertToInternationalCurrencySystem(currPrice)
-}
